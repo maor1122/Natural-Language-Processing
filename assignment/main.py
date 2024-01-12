2ec357e5f1bfc2f1c050df6a04677d0fe0ffdc11
name <maorsh74@gmail.com>
2024-01-12 07:05:38 +0200

    Fixed, now working also added comments.

--- assignment/main.py
+++ assignment/main.py
@@ -1,135 +1,123 @@
 from collections import defaultdict
 import json
-import pickle
-import os.path
+from time import time
+
+# Gets the candidates as a list from the file
+def get_candidates(candidates_file):
+    with open(candidates_file, 'r', encoding='utf-8') as fin:
+        return fin.read().split()
+
+# for each missing word in the cloze gets the previous and next word.
+def get_cloze_data(cloze_filename):
+    prev_words = list()
+    next_words = list()
+    with open(cloze_filename, 'r', encoding='utf-8') as cloze_file:
+        cloze = cloze_file.read().strip(',').lower().split()
+        n = len(cloze)
+        for i, word in enumerate(cloze):
+            if '__' in word:
+                if (i == 0 or cloze[i - 1][-1] == '.'):
+                    prev_words.append(".") # Since we cant check every word that ends with '.' in the corpus because of runtime constraints, if the word ends with '.' we ignore next word chance.
+                else:
+                    prev_words.append(cloze[i - 1])
+                if (i == n):
+                    next_words.append(".")
+                else:
+                    next_words.append(cloze[i + 1].replace('.', ''))
+    return prev_words, next_words
+
+# Gets the frequencies of the relevant words and pair words.
+def get_data_from_the_corpus(corpus, candidates):
+    print("reading data from corpus...")
+    with open(corpus, 'r', encoding='utf-8') as corpus_file:
+        previous = "."  # we'll mark '.' as </s>
+        words_dict = defaultdict(int)
+        corpus_lines = corpus_file.readlines()
+        for i, line in enumerate(corpus_lines):
+            for word in line.strip(',').lower().split():  # ignoring commas
+                stripped_word = word.strip('.')
+                if stripped_word in candidates:
+                    words_dict[stripped_word] += 1
+                    if previous == '.' or previous in candidates:
+                        words_dict[previous + " " + stripped_word] += 1
+                if word[-1] == '.':
+                    previous = '.'  # if last word ended with '.' we'll mark it as a new sentence instead of saving last word
+                    words_dict['.'] += 1
+                else:
+                    previous = stripped_word
+            if i % 100000 == 0:
+                print(i)
+        return words_dict
+
+#  calculates the probabilities of each candidate to each position, using the words_freq which contains relevant words and words pairs frequaecies
+def calculate_probabilities(words_freq, candidate_list, prev_words,next_words):
+    probablities = defaultdict(list)
+    for candidate in candidate_list:
+        for prev,next in zip(prev_words,next_words):
+            prev_chance = 0 if words_freq[prev+" "+candidate]==0 else words_freq[prev+" "+candidate]/words_freq[prev]  # P("{prev} {word}")/P({word}) = P(word|prev)
+            next_chance = 0 if words_freq[candidate+" "+next]==0 else words_freq[candidate+" "+next]/words_freq[next]  # P("{word} {next}")/P({next}) = P(word|next)
+            if next =='.':
+                next_chance=1  # Since we cant check every word that ends with '.' because of runtime constraints, if the word ends with '.' we ignore next word chance.
+            probablities[candidate].append(prev_chance*next_chance)
+    return probablities
 
 
 def solve_cloze(input, candidates, lexicon, corpus):
     # todo: implement this function
+    t1 = time()
     print(f'starting to solve the cloze {input} with {candidates} using {lexicon} and {corpus}')
-    if not os.path.isfile('pair_words2nfreq.pkl') or os.path.isfile('word2nfreq.pkl'):
-        pair_words_freq,words2nfreq = read_pair_words(corpus)
-        pickle.dump(pair_words_freq, open('pair_words2nfreq.pkl', 'wb'))
-        pickle.dump(pair_words_freq, open('word2nfreq.pkl', 'wb'))
+    prev_words, next_words = get_cloze_data(input)
+    candidate_list = get_candidates(candidates)
+    words_freq = get_data_from_the_corpus(corpus,prev_words+next_words+candidate_list)
+    probabilities = calculate_probabilities(words_freq, candidate_list, prev_words,next_words)
+    save_probablities(probabilities,"probabilities.json")
 
-    words_combinations = pickle.load(open('pair_words2nfreq.pkl', 'rb'))
-    words2nfreq = pickle.load(open('pair_words2nfreq.pkl', 'rb'))
-    lst = predict_trigram(words_combinations,words2nfreq, input, candidates)
+    lst = predict_best_combination(probabilities)
+
+    t2 = time()
+    print("time took: ", (t2 - t1) / 60, "minutes")
 
     return lst  # return your solution
 
-def predict_trigram(words_combinations,words2nfreq, input, candidates_file):
-    candidates = get_candidates(candidates_file)
-    print("candidantes: ", candidates)
-    probabilities = defaultdict(list)
-    with open(input, 'r', encoding='utf-8') as fin:
-        print('reading the input file...')
-        lines_list = fin.readlines()
-        for line_num, line in enumerate(lines_list):
-            words_list = line.split()
-            for i, word in enumerate(words_list):
-                if '___' in word:
-                    for candidate in candidates:
-                        if (i == 0):
-                            if (line_num == 0 or words_list[line_num-1][-1][-1]=='.'):  # If word is first
-                                prev_word = candidate
-                                word = words_list[i + 1].translate(str.maketrans("", "", '.,\n'))
-                                s = prev_word + " " + word
-                                probability = words_combinations[s] / words2nfreq[word]
-                                probabilities[candidate].append(probability)
-                                continue
-                            else:  # If word is first in line but not in the document
-                                prev_word = words_list[line_num-1][-1].translate(str.maketrans("", "", '.,\n'))
-                        else:  # Word isn't first in line
-                            prev_word = words_list[i - 1].translate(str.maketrans("", "", '.,\n'))
-                        word = candidate
-                        if(i == len(words_list)-1):
-                            if(line_num==len(lines_list)-1):  # If word is last
-                                s = prev_word + " " + word
-                                probability = words_combinations[s] / words2nfreq[prev_word]
-                                probabilities[candidate].append(probability)
-                                continue
-                            else:  # If word is last in line but not in the document
-                                next_word = lines_list[line_num+1][0].translate(str.maketrans("", "", '.,\n'))
-                        else:  # Word isn't last in line
-                            next_word = words_list[i+1].translate(str.maketrans("", "", '.,\n'))
-                        s_prev = prev_word + " " + word
-                        s_next = word + " " + next_word
-                        probability = (words_combinations[s_prev] / words2nfreq[prev_word])*(words_combinations[s_next] / words2nfreq[next_word])
-                        probabilities[candidate].append(probability)
-    save_probablities(probabilities)
-    return predict_best_combination(probabilities)
-
-def read_pair_words(filename):
-    prev_word = None
-    pair_words = defaultdict(int)
-    words_freq = defaultdict(int)
-    with open(filename, 'r', encoding='utf-8') as fin:
-        print('reading the text file...')
-        # read each line, removing commas points and newlines.
-        for i, line in enumerate([i.translate(str.maketrans("", "", '.,\n')) for i in fin.readlines()]):
-            for word in line.split():
-                if (prev_word == None):
-                    prev_word = word
-                    continue
-                else:
-                    pair_words[prev_word + " " + word] += 1
-                    words_freq[word]+=1
-                    prev_word = word
-            if i % 100000 == 0:
-                print(i)
-    return pair_words,words_freq
 
 
-def get_candidates(candidates_file):
-    with open(candidates_file, 'r', encoding='utf-8') as fin:
-        return [i.strip('\n') for i in fin.readlines()]
 
-
-def predict_bigram(words_combinations, input, candidates_file):
-    candidates = get_candidates(candidates_file)
-    print("candidantes: ",candidates)
-    probabilities = defaultdict(list)
-    total_words = sum(words_combinations.values())/2
-    with open(input, 'r', encoding='utf-8') as fin:
-        print('reading the input file...')
-        for line_num, line in enumerate([i.translate(str.maketrans("", "", '.,\n')) for i in fin.readlines()]):
-            words_list = line.split()
-            for i, word in enumerate(words_list):
-                if '___' in word:
-                    for candidate in candidates:
-                        s = words_list[i - 1] + " " + candidate
-                        probability = words_combinations[s] / total_words
-                        probabilities[candidate].append(probability)
-    save_probablities(probabilities)
-    return predict_best_combination(probabilities)
 
 
 def predict_best_combination(probabilities):
     n = len(probabilities.keys())
     final = [None] * n
+    filled_pos = list()
     adapted_probabilities = defaultdict(list)
     for candidate in probabilities.keys():
         prob_sum = sum(probabilities[candidate])
-        adapted_probabilities[candidate] = [i/prob_sum for i in probabilities[candidate]]
+        if(prob_sum!=0):
+            adapted_probabilities[candidate] = [i / prob_sum for i in probabilities[candidate]]
+        else:
+            adapted_probabilities[candidate] = [0 for i in probabilities[candidate]]
+    save_probablities(adapted_probabilities,'proportionate_probabilities.json')
     for _ in range(n):
         max_prob = float('-inf')
         for candidate in adapted_probabilities.keys():
             curr_max = max(adapted_probabilities[candidate])
-            if(curr_max>max_prob):
+            if (curr_max > max_prob):
                 max_prob = curr_max
                 candidate_max = candidate
                 pos_max = adapted_probabilities[candidate].index(curr_max)
+        adapted_probabilities.pop(candidate_max, None)
         for candidate in adapted_probabilities.keys():
             adapted_probabilities[candidate].pop(pos_max)
-        adapted_probabilities.pop(candidate_max,None)
-        while(final[pos_max]!=None):
-            pos_max+=1
+        for i in filled_pos:
+            if(pos_max>=i):
+                pos_max+=1
+        filled_pos.append(pos_max)
+        filled_pos.sort()
         final[pos_max] = candidate_max
     return final
 
-def save_probablities(probablities):
-    with open("probablities.json", "w") as outfile:
+
+def save_probablities(probablities,filename):
+    with open(filename, "w") as outfile:
         outfile.write(json.dumps(probablities, indent=2))
 
 
@@ -143,4 +131,3 @@ if __name__ == '__main__':
                            config['corpus'])
 
     print('cloze solution:', solution)
-
